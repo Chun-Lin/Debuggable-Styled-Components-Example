5c76a6adc507b3a250f4906ea648c83a77d623b4
Chun-Lin <wulin40063@gmail.com>
2018-08-22 18:16:45 +0800

    style: change color to skyblue

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import './content.css'
 
-export default ({ bold }) => {
+export default ({ skyblue }) => {
   /* inline-style */
   let style = {}
-  if (bold) {
-    style = { fontWeight: 'bold' }
+  if (skyblue) {
+    style = { color: 'skyblue' }
   }
 
   /* add new css classes */
   let titleStyles = ['content__title']
-  if (bold) {
-    titleStyles.push('content--bold')
+  if (skyblue) {
+    titleStyles.push('content--skyblue')
   }
 
   return (
     <div className="content">
       <div className="content__img" />
       <div className="content__info">
-        <div className="content__title">Cute Puppy</div>
+        <div className={titleStyles.join(' ')}>Cute Puppy</div>
         <div className="content__description">
           Sed ut voluptatem neque cumque. Qui sed ut itaque est doloribus qui.
           Eos perferendis autem qui fugiat.
