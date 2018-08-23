74b35ceb99902b929e619d10a0522f21393f9c85
Chun-Lin <wulin40063@gmail.com>
2018-08-23 15:05:05 +0800

    feat: styled-component version of original cute puppy style

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,29 +1,50 @@
 import React from 'react'
-import './content.css'
+import styled from 'styled-components'
+
+const ContentWrapper = styled.div`
+  width: 80%;
+  height: 300px;
+  box-shadow: 0 0 5px 2px #ccc;
+`
+
+const Img = styled.div`
+  display: inline-block;
+  width: 300px;
+  height: 100%;
+  background-image: url('https://picsum.photos/300/300?image=1062');
+`
+
+const InfoWrapper = styled.div`
+  display: inline-block;
+  vertical-align: top;
+  width: calc(100% - 300px);
+  height: 100%;
+  text-align: left;
+`
+
+const Title = styled.div`
+  padding: 20px 0 0 20px;
+  font-size: 48px;
+`
+
+const Description = styled.div`
+  padding: 20px;
+  font-size: 30px;
+  font-style: italic;
+  color: #888888;
+`
 
 export default ({ skyblue }) => {
-  /* inline-style */
-  let style = {}
-  if (skyblue) {
-    style = { color: 'skyblue' }
-  }
-
-  /* add new css classes */
-  let titleStyles = ['content__title']
-  if (skyblue) {
-    titleStyles.push('content--skyblue')
-  }
-
   return (
-    <div className="content">
-      <div className="content__img" />
-      <div className="content__info">
-        <div className={titleStyles.join(' ')}>Cute Puppy</div>
-        <div className="content__description">
+    <ContentWrapper>
+      <Img />
+      <InfoWrapper>
+        <Title>Cute Puppy</Title>
+        <Description>
           Sed ut voluptatem neque cumque. Qui sed ut itaque est doloribus qui.
           Eos perferendis autem qui fugiat.
-        </div>
-      </div>
-    </div>
+        </Description>
+      </InfoWrapper>
+    </ContentWrapper>
   )
 }
